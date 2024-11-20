d56c84f29bdbced759f0e6bb4ab2b497ee024bd8
scratchfree <longerbrown23@gmail.com>
2024-11-20 21:09:20 +0300

    kernel.py

---
+++ kernel.py
@@ -0,0 +1,19 @@
+
+def _welcome_request():
+    return print("Hi everyone! Initialize of success!")
+
+def _message_request(self):
+    return self
+    
+def _math_method(self, number_1, number_2):
+
+    self = number_1 + number_2
+    return print(f"{self}"), number_1, number_2
+    
+def _do_method_math(self):
+    return self
+
+
+if __name__ == '__main__':
+    _welcome_request()
+
